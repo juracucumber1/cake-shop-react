a8bd1bab415ef92781e28a58e2ca45e08805160f
bulhiCzar <bulhi.czar@gmail.com>
2022-10-22 22:51:42 +0300

    add update and remove

--- api/src/domain/product/product.service.ts
+++ api/src/domain/product/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import {Model, ObjectId} from 'mongoose';
 import {Product, ProductDocument} from "../../schema/product.schema";
 
 @Injectable()
@@ -15,5 +15,13 @@ export class ProductService {
     create(doc: Product) {
         const product = new this.productModel(doc)
         return product.save()
+    }
+
+    update(id: ObjectId, doc: Product) {
+        return this.productModel.findByIdAndUpdate(id, doc)
+    }
+
+    remove(id: ObjectId) {
+        return this.productModel.findByIdAndRemove(id)
     }
 }
